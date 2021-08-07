eee1187741a7cb633603eb5f3c66f8a8a0ebfca0
Tyler Shipe <tjshipe@gmail.com>
2021-08-07 15:05:53 -0400

    another test

--- stores/useWalletStore.tsx
+++ stores/useWalletStore.tsx
@@ -39,7 +39,7 @@ export const ENDPOINTS: EndpointInfo[] = [
     // programId: 'CRU6hX2GgtdabESgkoMswMrUdRFxHhCVYmS292VN1Nnn', // owned by governance
     //poolKey: 'GvSyVjGwLBeWdURMLDmSffQPqA8g547A6TURbbBnDpa4', // governance test
     // poolKey: '82ndgp58GXpwuLrEc9svHFdhiEsPaZoNUEWwgc79WHqk', // already over
-    poolKey: 'GZURkS3HhHor5KgnEodKwAZbzBi1j1bdjXiUpPtoLX5', // long deposits
+    poolKey: '4pdmNZ4AvjjbxfuLFh4gHcafPpvb9r7CDHP4hmpnFLip', // long deposits
     // poolKey: '7Dr2Ksnz5evoT9mEUgvvkmirH8KDC99b5oVPHbqSpx4K', // short deposit
     //poolKey: 'CdKyD4Qazo72Bm6SsPBWrT1AnH1NEuoUzvQg7b67EBac', // not started yet
   },
